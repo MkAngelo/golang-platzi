d6c9551aa4dfa8ec1c2cd94d3892936dcd8a00b6
MkAngelo <mikequi000@gmail.com>
2021-12-24 01:06:55 -0600

    Keywords

--- src/main.go
+++ src/main.go
@@ -3,22 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	switch modulo := 10 % 2; modulo {
-	case 0:
-		fmt.Println("PAR")
-	default:
-		fmt.Println("IMPAR")
-	}
+	// Defer
+	defer fmt.Println("Hola")
+	fmt.Println("Mundo")
 
-	// Sin condiciones
+	// Continue and break
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		if i == 4 {
+			fmt.Println("Es la mitad")
+			continue
+		}
 
-	value := 101
-	switch {
-	case value == 100:
-		fmt.Println("El valor es 100")
-	case value > 100:
-		fmt.Println("El valor es mayor a 100")
-	default:
-		fmt.Println("El valor es menor a 100")
+		if i == 7 {
+			break
+		}
 	}
 }
