85f02b0520c3c8fbd201f77a0fe652f4a83c26f4
MkAngelo <mikequi000@gmail.com>
2021-12-23 18:18:01 -0600

    For Loop

--- src/main.go
+++ src/main.go
@@ -2,31 +2,26 @@ package main
 
 import "fmt"
 
-func normalFunction(message string) {
-	fmt.Println(message)
-}
-
-func tripleArgument(a, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnValue(a int) int { // Valor de salida
-	return a * 2
-}
-
-func doubleReturn(a int) (c, d int) {
-	return a, a * 2
-}
-
 func main() {
-	normalFunction("Hola Mundo")
-	tripleArgument(1, 2, "H0l4!")
+	//For condicional
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
 
-	value := returnValue(10)
-	fmt.Println("Value: ", value)
+	fmt.Println("-----")
 
-	value1, value2 := doubleReturn(5)
-	fmt.Printf("Value1: %d, Value2: %d\n", value1, value2)
+	//For while
+	counter := 0
+	for counter < 10 {
+		fmt.Println(counter)
+		counter++
+	}
 
-	//PAra evitar usar una variable de retorno con '_'
+	fmt.Println("-----")
+
+	//For forever
+	counterForever := 0
+	for {
+		fmt.Println(counterForever)
+	}
 }
