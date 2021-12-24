cda7d8dbe0f02e8e59af83d51a2cf4facd0bfeeb
MkAngelo <mikequi000@gmail.com>
2021-12-23 19:41:18 -0600

    Reto Completado

--- src/main.go
+++ src/main.go
@@ -2,27 +2,25 @@ package main
 
 import "fmt"
 
+func isPair(number int) bool {
+	if number%2 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func validatePass(pass, confPass string) string {
+	message := "No coinciden"
+	if pass == confPass {
+		message = "Coinciden"
+	}
+	return message
+}
+
 func main() {
-	valor1 := 2
-	valor2 := 2
-
-	if valor1 == 1 {
-		fmt.Println("El valor es 1")
-	} else {
-		fmt.Println("El valor NO es 1")
-	}
-
-	// With and
-	if valor1 == 1 && valor2 == 2 {
-		fmt.Println("Es Verdad")
-	} else {
-		fmt.Println("Es Mentira")
-	}
-
-	// With and
-	if valor1 == 1 || valor2 == 2 {
-		fmt.Println("Es Verdad")
-	} else {
-		fmt.Println("Es Mentira")
-	}
+	result := isPair(5)
+	fmt.Println(result)
+	password := validatePass("Hola", "hola")
+	fmt.Println(password)
 }
