d9ea3ed07d1ece143e79de754cad075d7fe802b1
MkAngelo <mikequi000@gmail.com>
2021-12-30 17:24:57 -0600

    struct

--- src/main.go
+++ src/main.go
@@ -2,33 +2,20 @@ package main
 
 import "fmt"
 
+type car struct {
+	brand string
+	year  int
+}
+
 func main() {
-	//Declaracion de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.1415
+	myCar := car{brand: "Ford", year: 2020}
+	fmt.Println(myCar)
 
-	fmt.Println("pi", pi)
-	fmt.Println("pi2", pi2)
+	// Otra manera
 
-	// Declaracion de variables enteras
+	var otherCar car
+	otherCar.brand = "Ferrari"
 
-	base := 2           //No ha sido declarada anteriormente creada e iniciada sin tipo de dato
-	var altura int = 14 // declaramos la variable y asignamos un valor con el tipo de variable
-	var area int        // solo la declaramos
-
-	fmt.Println(base, altura, area)
-
-	// Zero values
-	var a int     // 0
-	var b float64 // 0
-	var c string  // ''
-	var d bool    // false
-	fmt.Println(a, b, c, d)
-
-	// Area de un cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-
-	fmt.Println("Area cuadrado: ", areaCuadrado)
+	fmt.Println(otherCar)
 
 }
