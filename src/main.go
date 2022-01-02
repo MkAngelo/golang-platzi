15cf79c94a7964b494a65506db9398f93fadb342
MkAngelo <mikequi000@gmail.com>
2022-01-02 00:06:52 -0600

    range close and select

--- src/main.go
+++ src/main.go
@@ -4,16 +4,38 @@ import (
 	"fmt"
 )
 
-func say(text string, c chan<- string) {
+func message(text string, c chan string) {
 	c <- text
 }
 
 func main() {
-	c := make(chan string, 1)
+	c := make(chan string, 2)
+	c <- "Message1"
+	c <- "Message2"
 
-	fmt.Println("Hello")
+	fmt.Println(len(c), cap(c))
 
-	go say("Bye", c)
+	// Range and close
+	close(c)
 
-	fmt.Println(<-c)
+	// c <- "Mensaje"
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	// Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("Mensaje1", email1)
+	go message("Mensaje2", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email2", m2)
+		}
+	}
 }
