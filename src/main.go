23f75ca3c60c12766ce34d35db772a138da05870
MkAngelo <mikequi000@gmail.com>
2022-01-01 22:14:19 -0600

    Funcion Show con Strings

--- src/main.go
+++ src/main.go
@@ -16,6 +16,11 @@ func (myPc *pc) duplicateRAM() {
 	myPc.ram = myPc.ram * 2
 }
 
+func (myPC pc) show() {
+	showString := fmt.Sprintf("Esta PC es de la marca %s, tiene %d GB de RAM y %d GB de disco.", myPC.brand, myPC.ram, myPC.disk)
+	fmt.Println(showString)
+}
+
 func main() {
 	a := 50
 	b := &a // Puntero a memoria
@@ -28,8 +33,8 @@ func main() {
 	fmt.Println(a)
 
 	myPC := pc{ram: 16, disk: 200, brand: "HP"}
-	fmt.Println(myPC)
 
+	myPC.show()
 	myPC.ping()
 
 	fmt.Println(myPC)
