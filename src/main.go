ee2abc506deeecae1cd08b96f22cda886024964c
MkAngelo <mikequi000@gmail.com>
2022-01-01 21:52:34 -0600

    Reto 2 Completado

--- src/main.go
+++ src/main.go
@@ -1,34 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"curso_golang_platzi/src/mypc"
+	"fmt"
+)
+
+func newPC() {
+	var myPC mypc.PcSettings
+	myPC.Brand = "Lenovo"
+	myPC.RAM = 12
+	myPC.Disk = 500
+	myPC.Year = 2020
+
+	fmt.Println(myPC)
+}
 
 func main() {
-	//Declaracion de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.1415
-
-	fmt.Println("pi", pi)
-	fmt.Println("pi2", pi2)
-
-	// Declaracion de variables enteras
-
-	base := 2           //No ha sido declarada anteriormente creada e iniciada sin tipo de dato
-	var altura int = 14 // declaramos la variable y asignamos un valor con el tipo de variable
-	var area int        // solo la declaramos
-
-	fmt.Println(base, altura, area)
-
-	// Zero values
-	var a int     // 0
-	var b float64 // 0
-	var c string  // ''
-	var d bool    // false
-	fmt.Println(a, b, c, d)
-
-	// Area de un cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-
-	fmt.Println("Area cuadrado: ", areaCuadrado)
-
+	newPC()
 }
---
+++ src/mypc/mypc.go
@@ -0,0 +1,9 @@
+package mypc
+
+// PCSettings es un Struct acerca de computadoras
+type PcSettings struct {
+	Brand string
+	RAM   uint
+	Disk  uint
+	Year  uint
+}
