e9e2ab1973f86b1f15e5a2730e8528d08410eaa6
MkAngelo <mikequi000@gmail.com>
2022-01-01 19:58:11 -0600

    Punteros

--- src/main.go
+++ src/main.go
@@ -1,16 +1,40 @@
 package main
 
-import (
-	"curso_golang_platzi/src/mypackage"
-	"fmt"
-	// pk "ruta" es la forma de poner alias
-)
+import "fmt"
+
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPC pc) ping() {
+	fmt.Println(myPC.brand, "Pong.")
+}
+
+func (myPc *pc) duplicateRAM() {
+	myPc.ram = myPc.ram * 2
+}
 
 func main() {
-	var myCar mypackage.CarPublic
-	myCar.Brand = "Ferrari"
-	myCar.Year = 2001
-	// Solo se  pude acceder a los paquetes publicos
+	a := 50
+	b := &a // Puntero a memoria
 
-	fmt.Println(myCar)
+	fmt.Println(a)
+	fmt.Println(*b) // Accedemos al valor en memoria
+	fmt.Println(b)  // Accedemos al puntero
+
+	*b = 100
+	fmt.Println(a)
+
+	myPC := pc{ram: 16, disk: 200, brand: "HP"}
+	fmt.Println(myPC)
+
+	myPC.ping()
+
+	fmt.Println(myPC)
+	myPC.duplicateRAM()
+	fmt.Println(myPC)
+	myPC.duplicateRAM()
+	fmt.Println(myPC)
 }
